403f46645993b4fe27a11c758b4dd86dfcc8bcea
Bhuvnesh Sain <bhuvnesh.sain2016@rnbglobal.ac.in>
2023-05-18 23:56:39 +0530

    New

---
+++ Form8.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form8
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(27, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(145, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Number of elements"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(178, 52)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(70, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(92, 111)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(114, 37)
+        Button1.TabIndex = 2
+        Button1.Text = "Print"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(27, 187)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "Series: "
+        ' 
+        ' Form8
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(305, 279)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form8"
+        Text = "Form8"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form8.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form8
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim n As Integer = Val(TextBox1.Text)
+        Dim n0 As Integer = 0
+        Dim n1 As Integer = 1
+        Dim i As Integer = 1
+        Dim n2 As Integer
+        Dim str As String = n0.ToString() + " " + n1.ToString()
+
+
+        While i <= n
+
+            n2 = n0 + n1
+            str = str + " " + n2.ToString()
+            n0 = n1
+            n1 = n2
+            i = i + 1
+
+        End While
+
+        Label2.Text = str
+
+
+    End Sub
+End Class
