f7b206ab2c11957120787d47eadacf85a33d9f78
Bhuvnesh Sain <bhuvnesh.sain2016@rnbglobal.ac.in>
2023-05-18 16:10:46 +0530

    First

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,85 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(165, 74)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(75, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(54, 81)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "A="
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(54, 126)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(28, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "B="
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(165, 119)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(75, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(86, 223)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(102, 179)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(57, 20)
+        Label3.TabIndex = 5
+        Label3.Text = "Answer"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(281, 287)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
 
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label3.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+
+    End Sub
 End Class
